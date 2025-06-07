113f1c3b0e9d07ca5ad52e3bee647c4eaa76ff89
sogeking-nul <utsav.jana.5@gmail.com>
2025-06-07 22:25:22 +0530

    added gift contract

---
+++ contractFactory/src/GiftContract.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {ReentrancyGuard} from "contractFactory/lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+
+contract GiftContract is ReentrancyGuard{
+    //events
+    event FundsRecieved(address indexed buyer, uint256 amount, uint256 timestamp);
+
+    address immutable private i_factoryAddress;
+    address immutable private i_owner;
+    address immutable private i_buyer;
+    uint256 immutable private i_amount;
+    bool public s_isFullfilled;
+        
+    constructor(address _from, address _owner, uint256 _amount){
+        require(_amount>0, "GiftCard value cant be zero");
+        i_buyer = _from;
+        i_amount = _amount;
+        i_owner = _owner;
+        i_factoryAddress = msg.sender;
+        s_isFullfilled = false;
+    }   
+
+    receive() external payable{
+        fullfill();
+    }
+
+    function fullfill() public payable {
+        require(msg.value == i_amount, "Incorrect amount sent");
+        require(msg.sender == i_buyer, "Only the buyer can send funds");
+        s_isFullfilled = true;        
+        (bool success, ) = payable(i_owner).call{value: msg.value}("");
+        require(success, "Transfer failed");
+        emit FundsRecieved(msg.sender, msg.value, block.timestamp);
+    }
+
+    //getters
+    function getFactoryAddress() external view returns(address){
+        return i_factoryAddress;
+    }
+    function getOwner() external view returns(address){
+        return i_owner;
+    }
+    function getBuyer() external view returns(address){
+        return i_buyer;
+    }
+    function getAmount() external view returns(uint256){
+        return i_amount;
+    }
+    function isFullfilled() external view returns(bool){
+        return s_isFullfilled;
+    }
+}
